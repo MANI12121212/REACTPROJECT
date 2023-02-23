a931a00729ec9cf449a3dbd2fd3dd5fe1cc59268
MANI12121212 <101326938+MANI12121212@users.noreply.github.com>
2023-02-23 17:26:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+ 
 import './App.css';
-
+import Card  from './Card';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='inner'>
+     <Card colorname="PINK" colorCode="#FF6663" />
+     <Card colorname="GREY" colorCode="#333" />
+     <Card colorname="BLACK" colorCode="#000" />
+     <Card colorname="GREEN" colorCode="#38BB14" />
+     <Card colorname="RED" colorCode="#C90B0B" />
+     <Card colorname="ORANGE" colorCode="#FF8000" />
+     <Card colorname="YELLOW" colorCode="#FFF500" />
+     <Card colorname="LIGHT GREY" colorCode="#CCCC" />
+     <Card colorname="PURPLE" colorCode="#7E41A2" />
+     <Card colorname="BROWN" colorCode="#C14911" />
+       </div>
     </div>
   );
 }
